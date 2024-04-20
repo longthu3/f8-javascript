bcb904d6c2346dc0661582e440e4face2b3aefc8
duylongtd <tranlong280403@gmail.com>
2024-04-21 02:03:24 +0700

    remove: git ignore

---
+++ constant/config.js
@@ -0,0 +1,19 @@
+const CLOUDINARY_PRESET = "ml_default";
+const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dlrisakha/image/upload";
+const MOCK_DATA = [
+    {
+        id: 'lesson16',
+        label: 'Lesson 16'
+    },
+    {
+        id: 'lesson17',
+        label: 'Lesson 17'
+    },
+    {
+        id: 'lesson18',
+        label: 'Lesson 18'
+    }
+];
+
+
+export { CLOUDINARY_PRESET, CLOUDINARY_URL, MOCK_DATA };
