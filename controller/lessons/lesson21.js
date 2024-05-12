044649708a47f018eac925b7f67227e70e6a6e65
duylongtd <tranlong280403@gmail.com>
2024-05-12 18:16:36 +0700

    feat: lesson 21, question 4

--- controller/lessons/lesson21.js
+++ controller/lessons/lesson21.js
@@ -1,22 +1,38 @@
-const globalArr = [4, 4, 8, 4, 10];
+const numbers = [5, 1, 9, 8, 10];
 
-const filterDuplicateElements = (arr) => {
-    let result = [];
-    arr.forEach((item) => {
-        if (!includes(result, item)) {
-            result.push(item);
-        }
-    });
-    return result;
+//arr[i] = num
+//i + 1 = arr[i]
+// 5 3 
+
+
+const element = 4;
+
+const insertElement = (arr, element, index) => {
+    const length = arr.length;
+    for (let i = length; i > index; --i) {
+        arr[i] = arr[i - 1];
+    }
+    arr[index] = element;
+    return arr;
 }
 
-const includes = (arr, item) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === item) {
-            return true;
+
+const bubbleSort = (arr) => {
+    const length = arr.length;
+
+    for (let i = 0; i < length - 1; ++i) {
+        for (let j = i + 1; j < length; ++j) {
+            if (arr[i] > arr[j]) {
+                const temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
-    return false;
+    return arr;
 }
 
-console.log(filterDuplicateElements(globalArr));
+const sortedNumber = bubbleSort(numbers);
+
+console.log(sortedNumber);
+console.log(insertElement(sortedNumber, 22, 2));
